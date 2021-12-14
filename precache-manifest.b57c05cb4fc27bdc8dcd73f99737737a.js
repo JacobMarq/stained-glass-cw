6be021d5476b6965b7faff40008ae6e372d8bc18
Jacob <Jacobmarq@gmail.com>
2021-12-14 13:23:44 -0800

    Updates

---
+++ precache-manifest.b57c05cb4fc27bdc8dcd73f99737737a.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82c7466340755015853e9164343e47d",
+    "url": "/stained-glass-cw/index.html"
+  },
+  {
+    "revision": "de6b65c47fc5d5673336",
+    "url": "/stained-glass-cw/static/css/2.5a56e992.chunk.css"
+  },
+  {
+    "revision": "508bff507d129ce4094f",
+    "url": "/stained-glass-cw/static/css/main.cf0d956a.chunk.css"
+  },
+  {
+    "revision": "de6b65c47fc5d5673336",
+    "url": "/stained-glass-cw/static/js/2.e00d058b.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/stained-glass-cw/static/js/2.e00d058b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde429502cd74cdf860a",
+    "url": "/stained-glass-cw/static/js/3.782133f3.chunk.js"
+  },
+  {
+    "revision": "508bff507d129ce4094f",
+    "url": "/stained-glass-cw/static/js/main.34c53503.chunk.js"
+  },
+  {
+    "revision": "2f2b9fe86490e324d7d5",
+    "url": "/stained-glass-cw/static/js/runtime-main.f82a8bd0.js"
+  },
+  {
+    "revision": "d567173ad93a9575c86c371381bc4273",
+    "url": "/stained-glass-cw/static/media/1.d567173a.jpg"
+  },
+  {
+    "revision": "ab913ae80951879bce4fd165cbf61331",
+    "url": "/stained-glass-cw/static/media/10.ab913ae8.jpg"
+  },
+  {
+    "revision": "ae9463d67fa30da2574b34c8834832dc",
+    "url": "/stained-glass-cw/static/media/11.ae9463d6.jpg"
+  },
+  {
+    "revision": "73b7b029d3a8a020826d954cfef84a24",
+    "url": "/stained-glass-cw/static/media/12.73b7b029.jpg"
+  },
+  {
+    "revision": "4135ccfb351598f855fcf971890c7521",
+    "url": "/stained-glass-cw/static/media/13.4135ccfb.jpg"
+  },
+  {
+    "revision": "586bc94275d6cc6fc86460d8045f754a",
+    "url": "/stained-glass-cw/static/media/14.586bc942.jpg"
+  },
+  {
+    "revision": "b360d2c36448fad469681872aba1c3a7",
+    "url": "/stained-glass-cw/static/media/15.b360d2c3.jpg"
+  },
+  {
+    "revision": "0a458a20c32dfb4ce8934fb2df0bd1a1",
+    "url": "/stained-glass-cw/static/media/16.0a458a20.jpg"
+  },
+  {
+    "revision": "03cf4dde6cbfca9f56329cdc2f7055e6",
+    "url": "/stained-glass-cw/static/media/17.03cf4dde.jpg"
+  },
+  {
+    "revision": "817c00429109ce32af526b2ad11dccf6",
+    "url": "/stained-glass-cw/static/media/2.817c0042.jpg"
+  },
+  {
+    "revision": "0f69732cef36f36ddd99af40f9c43219",
+    "url": "/stained-glass-cw/static/media/4.0f69732c.jpg"
+  },
+  {
+    "revision": "c1018f76cb6768ed9bb2d891b45cb241",
+    "url": "/stained-glass-cw/static/media/5.c1018f76.jpg"
+  },
+  {
+    "revision": "224b57d20dbd17e3ce77c56d92d3a09a",
+    "url": "/stained-glass-cw/static/media/6.224b57d2.jpg"
+  },
+  {
+    "revision": "ef76e9531df3a0f992624ddf373ef7c5",
+    "url": "/stained-glass-cw/static/media/7.ef76e953.jpg"
+  },
+  {
+    "revision": "fc73b63622ebf6dd46320da077bd60ae",
+    "url": "/stained-glass-cw/static/media/8.fc73b636.jpg"
+  },
+  {
+    "revision": "6245918f01c638c603d46e78f1ddf86d",
+    "url": "/stained-glass-cw/static/media/9.6245918f.jpg"
+  },
+  {
+    "revision": "4600981cbdd5923d2a70772f811b886d",
+    "url": "/stained-glass-cw/static/media/Custom.4600981c.png"
+  },
+  {
+    "revision": "99f77cc2fece6f7609c28810c01e6df0",
+    "url": "/stained-glass-cw/static/media/Licensed.99f77cc2.png"
+  },
+  {
+    "revision": "ffbe00346202a05e2f373a11632561e2",
+    "url": "/stained-glass-cw/static/media/Mail.ffbe0034.png"
+  },
+  {
+    "revision": "ef26de4cf16d184b2ee15c168f932f08",
+    "url": "/stained-glass-cw/static/media/PhoneCall.ef26de4c.png"
+  },
+  {
+    "revision": "7cd65aa62deebfbe424caf7287c8af99",
+    "url": "/stained-glass-cw/static/media/StainedGlassCW.7cd65aa6.jpg"
+  },
+  {
+    "revision": "085d58a9ee58fe0d3e1414520111f573",
+    "url": "/stained-glass-cw/static/media/StainedGlassCWIcon.085d58a9.jpg"
+  },
+  {
+    "revision": "2c80c64d208edf2cd92ae6c08017bb5e",
+    "url": "/stained-glass-cw/static/media/StainedGlassCWText.2c80c64d.jpg"
+  },
+  {
+    "revision": "60a6a8e86aebfeadf2a40af8dada68cb",
+    "url": "/stained-glass-cw/static/media/Variety.60a6a8e8.png"
+  },
+  {
+    "revision": "39adb0e000308e71b9e6d069c1448e45",
+    "url": "/stained-glass-cw/static/media/arch-window.39adb0e0.jpg"
+  },
+  {
+    "revision": "6ad854eabc0b12a1f05ab5b5fe4f06d5",
+    "url": "/stained-glass-cw/static/media/casement.6ad854ea.jpg"
+  },
+  {
+    "revision": "82bc5876edcfadb97b5422ae47cc61dd",
+    "url": "/stained-glass-cw/static/media/double-hung.82bc5876.jpg"
+  },
+  {
+    "revision": "462cd8e2e3c7a492a5554669e7fab9bb",
+    "url": "/stained-glass-cw/static/media/folding.462cd8e2.jpg"
+  },
+  {
+    "revision": "b6dbd1b0b03f8b552dfdc8ddeba0f3df",
+    "url": "/stained-glass-cw/static/media/french-swing.b6dbd1b0.jpg"
+  },
+  {
+    "revision": "aab70d15fd3c6e7eaba6a726211d4963",
+    "url": "/stained-glass-cw/static/media/horizontal-sliding.aab70d15.jpg"
+  },
+  {
+    "revision": "61d347bc842b45093528b8d9cd315be8",
+    "url": "/stained-glass-cw/static/media/multi-slide.61d347bc.jpg"
+  },
+  {
+    "revision": "5857bcbe3b280b955ef22d77103a59b2",
+    "url": "/stained-glass-cw/static/media/picture-window.5857bcbe.jpeg"
+  },
+  {
+    "revision": "a444f7441a038f0942b7a911c8da92b4",
+    "url": "/stained-glass-cw/static/media/px1.a444f744.jpg"
+  },
+  {
+    "revision": "ccb377779150532654ab3da0f5a7e0e3",
+    "url": "/stained-glass-cw/static/media/single-hung.ccb37777.jpg"
+  },
+  {
+    "revision": "746e01fff482980cbfcfb8eeeb528e61",
+    "url": "/stained-glass-cw/static/media/sliding-door.746e01ff.jpg"
+  }
+]);
